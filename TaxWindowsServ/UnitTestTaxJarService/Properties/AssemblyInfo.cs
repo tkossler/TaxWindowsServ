6bef3834309602b9f3d27edfd9c7ab87b1b7957d
tkoss <tkoss@allstate.com>
2020-11-06 12:00:44 -0500

    Nov 6 2020
    Nov 6  2020

---
+++ TaxWindowsServ/UnitTestTaxJarService/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestTaxJarService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Allstate Insurance Company")]
+[assembly: AssemblyProduct("UnitTestTaxJarService")]
+[assembly: AssemblyCopyright("Copyright © Allstate Insurance Company 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("759419df-eb24-4e3f-81d7-eca0a18dfe1e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
